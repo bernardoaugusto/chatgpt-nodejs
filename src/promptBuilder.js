f94f4c70e554433211c7f2c17639015274989222
Bernardo Bastos <bernardo.augusto.bastos@gmail.com>
2023-05-30 19:24:49 -0300

    refactor: separete files

--- src/promptBuilder.js
+++ src/promptBuilder.js
@@ -9,16 +9,18 @@ export default (title, description) => {
   ​
     E formate sua resposta em formato JSON de forma retornar um boolean se a vaga é compatível ou não e com as hard skills presentes no texto, exemplo de retorno desejado:
    
+    {
     "hard_skills_compativel": false,
     "nao_precisa_de_grande_experiencia": false,
     "nao_precisa_de_graduacao": false,
     "nao_precisa_de_ingles_avancado": false,
     "hard_skills": ["PHP", "JavaScript"]
+    }
    
   ​
     Regra: sempre retornar apenas um json como resposta.
   ​
     Título da vaga: ${title}
     Descrição da vaga: ${description}
-   `
-}
+   `;
+};
